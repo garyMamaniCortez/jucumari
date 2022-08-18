ab396befea6ceb8cf1c30db2ab57502218bca716
garyMamaniCortez <mamanijhonny2210@gmail.com>
2022-08-18 18:03:30 -0400

    deploy v1.0.0

--- tracker-web/src/environments/environment.prod.ts
+++ tracker-web/src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+
+    apiKey: "AIzaSyDdFXUkAYYt6J8w07jctuW3spjepFaRwFA",
+
+    authDomain: "tracker-2c9c8.firebaseapp.com",
+
+    projectId: "tracker-2c9c8",
+
+    storageBucket: "tracker-2c9c8.appspot.com",
+
+    messagingSenderId: "675057084203",
+
+    appId: "1:675057084203:web:1551d51062bc504e29076e",
+
+    measurementId: "G-24HKY6TB33"
+
+  }
 };
